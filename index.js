c6d49aefbbd85118962891498e8343554869e274
Thomas Mach <m4chtomasz@gmail.com>
2025-04-15 14:23:32 +0200

    add game logic

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-console.log("Black Jack");
-
 const getCardBtn = document.getElementById("btn-get-card");
+const standBtn = document.getElementById("btn-stand");
 const dealerCardsDIV = document.getElementById("dealer-cards");
 const playerCardsDIV = document.getElementById("player-cards");
+const resetBtn = document.getElementById("reset");
 
 let deckId = null;
 let dealerCards = [];
@@ -19,7 +19,7 @@ const getDeck = async () => {
       throw new Error("There was a problem with the API");
     }
     const data = await response.json();
-    console.log(data);
+    // console.log(data);
     return data.deck_id;
   } catch (err) {
     console.log(err);
@@ -41,9 +41,18 @@ const playerHand = async () => {
     }
     const data = await response.json();
     playerCards = data.cards;
-    console.log("playerCards", playerCards);
+    console.log("PlayerCards", playerCards);
+    console.log("Player Cards Points", calcPoints(getPoints(playerCards)));
     renderCards(playerCardsDIV, playerCards);
-    console.log(data);
+    let points = calcPoints(getPoints(playerCards));
+    if (points === 21) {
+      console.log(`Player Cards points:", ${points} BlacJack`);
+      getCardBtn.disabled = true;
+      standBtn.disabled = true;
+      dealerPlay();
+      return;
+    }
+    // console.log(data);
   } catch (err) {
     console.log(err);
   }
@@ -56,7 +65,7 @@ const dealerHand = async () => {
   }
   try {
     const response = await fetch(
-      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=1`
+      `https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`
     );
     if (!response.ok) {
       // Checks the succsess of HTTP response status, whitch might not trow an erro but still indicates a faliure
@@ -65,8 +74,9 @@ const dealerHand = async () => {
     const data = await response.json();
     dealerCards = data.cards;
     console.log("Dealer Cards", dealerCards);
+    console.log("Dealer Cards Points", calcPoints(getPoints(dealerCards)));
     renderCards(dealerCardsDIV, dealerCards);
-    console.log(data);
+    // console.log(data);
   } catch (err) {
     console.log(err);
   }
@@ -78,12 +88,33 @@ const dealerHand = async () => {
   playerHand();
 })();
 
-getCardBtn.addEventListener("click", () => {
+getCardBtn.addEventListener("click", async () => {
   if (!isFetchingCard) {
     isFetchingCard = true;
-    getCard(playerCards, playerCardsDIV);
-    getCard(dealerCards, dealerCardsDIV);
+    let points = calcPoints(getPoints(playerCards));
+    if (points < 21) {
+      await getCard(playerCards, playerCardsDIV);
+      points = calcPoints(getPoints(playerCards));
+      console.log("Player Cards points:", points);
+    }
+    if (points >= 21) {
+      console.log(`Player Cards points:", ${points} sballo o 21`);
+      getCardBtn.disabled = true;
+      standBtn.disabled = true;
+      dealerPlay();
+      return;
+    }
   }
+});
+
+standBtn.addEventListener("click", () => {
+  getCardBtn.disabled = true;
+  standBtn.disabled = true;
+  dealerPlay(calcPoints(getPoints(dealerCards)));
+});
+
+resetBtn.addEventListener("click", () => {
+  location.reload();
 });
 
 function renderCards(element, cards) {
@@ -108,11 +139,72 @@ async function getCard(forWhoArr, domElement) {
     }
     const data = await response.json();
     forWhoArr.push(data.cards[0]);
-    console.log(`Cards for element`, domElement, forWhoArr);
+    // console.log(`Cards for element`, domElement, forWhoArr);
+    console.log(getPoints(forWhoArr));
     renderCards(domElement, forWhoArr);
     isFetchingCard = false;
-    console.log(data);
+    // console.log(data);
   } catch (err) {
     console.log(err);
+  }
+}
+
+function getPoints(cardsArr) {
+  return cardsArr.map((card) => {
+    let val = card.value;
+    if (["JACK", "QUEEN", "KING"].includes(val)) {
+      return 10;
+    } else if (val === "ACE") {
+      return 11;
+    } else {
+      return Number(val);
+    }
+  });
+}
+
+function calcPoints(pointsArr) {
+  let total = pointsArr.reduce((tot, el) => tot + el);
+  let aces = pointsArr.filter((el) => el === 11).length;
+
+  while (total > 21 && aces > 0) {
+    total -= 10;
+    aces--;
+  }
+  return total;
+}
+
+async function dealerPlay(points) {
+  console.log(`dealer play run...`);
+  points = calcPoints(getPoints(dealerCards));
+  if (points < 17) {
+    await getCard(dealerCards, dealerCardsDIV);
+    calcPoints(getPoints(dealerCards));
+    await dealerPlay(points);
+  } else if (points >= 17) {
+    checkWinner(
+      calcPoints(getPoints(playerCards)),
+      calcPoints(getPoints(dealerCards))
+    );
+  }
+  points = calcPoints(getPoints(dealerCards));
+}
+
+function checkWinner(player, dealer) {
+  if (player > 21) {
+    console.log(`player:${player}, dealer:${dealer}`);
+    return console.log(`dealer win`);
+  }
+
+  if (dealer < player || dealer > 21) {
+    console.log(`player:${player}, dealer:${dealer}`);
+    return console.log(`player win`);
+  }
+
+  if (dealer > player && dealer < 22) {
+    console.log(`player:${player}, dealer:${dealer}`);
+    return console.log(`dealer win`);
+  } else {
+    console.log(`player:${player}, dealer:${dealer}`);
+    return console.log(`pareggio`);
   }
 }
